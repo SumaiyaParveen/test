b4bdd2017925af63e827e84a7677fee131963fcc
Sumaiya Parveen muthair basha <smuthair@my.centennialcollege.ca>
2023-06-26 11:22:17 -0400

    commit 3

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'home' });
+});
+
+/* GET home page. */
 router.get('/home', function(req, res, next) {
   res.render('index', { title: 'home' });
 });
