abc6bcf533c2b4338e4dd1be841009a42f1d1a61
Sumaiya Parveen muthair basha <smuthair@my.centennialcollege.ca>
2023-06-26 13:13:57 -0400

    your commit

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'home' });
+  res.render('index', { title: 'home', username: '' });
 });
 
 /* GET home page. */
 router.get('/home', function(req, res, next) {
-  res.render('index', { title: 'home' });
+  res.render('index', { title: 'home' , username: ''});
 });
 
 /* GET about page. */
+
 router.get('/about', function(req, res, next) {
   res.render('aboutme', { title: 'About me' });
 });
@@ -31,4 +32,10 @@ router.get('/contactme', function(req, res, next) {
   res.render('contactme', { title: 'contactme' });
 });
 
+// insert name to the home page
+router.post('/contactme', (req, res, next) => {
+  //getting data from form
+  let newName =req.body.name;
+  res.render('index', {title : 'home' , username : 'Hi ' +newName+', '})
+});
 module.exports = router;
